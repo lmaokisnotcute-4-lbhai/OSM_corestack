891fa50087e2b2879ffccf1dfdbc37ad80962ea8
DevanshiMalik <157407617+lmaokisnotcute-4-lbhai@users.noreply.github.com>
2026-04-07 00:33:53 +0530

    Update geosimplify.py

--- scripts/geosimplify.py
+++ scripts/geosimplify.py
@@ -2,14 +2,21 @@ from shapely.geometry import shape
 from shapely.ops import unary_union
 import json
 
-with open("input.geojson") as f:
+watershed_id = input("Enter watershed ID: ").strip()
+
+input_file = f"{watershed_id}_microwatersheds.geojson"
+output_file = f"{watershed_id}_simplified.geojson"
+
+with open(input_file) as f:
     data = json.load(f)
 
-# Simplify geometry
+
 for feature in data["features"]:
     geom = shape(feature["geometry"])
-    simplified = geom.simplify(0.00005, preserve_topology=True)
+    simplified = geom.simplify(0.00002, preserve_topology=True)
     feature["geometry"] = json.loads(json.dumps(simplified.__geo_interface__))
 
-with open("simplified.geojson", "w") as f:
+with open(output_file, "w") as f:
     json.dump(data, f)
+
+print(f"\n✅ Simplified file saved as: {output_file}")
